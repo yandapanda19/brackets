f6aa9e0b57d4a9c8237681732afbcb9d311dd180
yandapanda19 <i.am.kitiket@gmail.com>
2023-01-20 00:47:17 +0200

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let brackets = {};
+  for (let elem of bracketsConfig) {
+    brackets[elem[1]] = elem[0];
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (brackets[str[i]] === str[i]) {
+      if (stack[stack.length - 1] !== str[i]) {
+        stack.push(str[i]);
+      } else {
+        if (brackets[str[i]] !== stack.pop()) {
+          return false;
+        }
+      }
+    }
+
+    if (Object.keys(brackets).includes(str[i]) && brackets[str[i]] !== str[i]) {
+      if (brackets[str[i]] !== stack.pop()) {
+        return false;
+      }
+    }
+    if (
+      !Object.keys(brackets).includes(str[i]) &&
+      brackets[str[i]] !== str[i]
+    ) {
+      stack.push(str[i]);
+    }
+  }
+
+  return stack.length === 0;
+};
